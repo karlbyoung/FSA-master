3cf302294e7d47cbde1ab85b43390fe98bc0857d
Karl Young <karl.young@interworks.com>
2024-04-02 14:36:23 -0600

    Update ROW_NO persistence and FR_PR_DAYS

--- Snowflake/1_DEMAND_AND_SUPPLY/DEMAND_PO.sql
+++ Snowflake/1_DEMAND_AND_SUPPLY/DEMAND_PO.sql
@@ -1,13 +1,17 @@
 CREATE OR REPLACE TABLE DEV.${vj_fsa_schema}.DEMAND_PO AS
 (
   SELECT distinct dpo.* EXCLUDE PK_ID,
-  	"UNIQUE_KEY"||'^'||ZEROIFNULL("COMPONENT_ITEM_ID")::TEXT PK_ID,
+  	dpo.UNIQUE_KEY||'^'||ZEROIFNULL(COMPONENT_ITEM_ID)::TEXT PK_ID,
   	prev.ROW_NO
   FROM DEV.${vj_fsa_schema}.DEMAND_PO_ALL dpo
   LEFT OUTER JOIN (
+      SELECT DISTINCT
+        ORDER_NUMBER, 
       /* 20230724 - KBY, RFS23-1850 - keep ROW_NO low for duplicate order numbers */
-      SELECT DISTINCT ORDER_NUMBER, MIN(ROW_NO) OVER (PARTITION BY ORDER_NUMBER) ROW_NO 
+        MIN(ROW_NO) OVER (PARTITION BY ORDER_NUMBER) ROW_NO,
+      /* 20240401 -- KBY - match on unique_key, not order_number */
+        UNIQUE_KEY
       FROM DEV.${vj_fsa_schema}.DEMAND_PREV_ASSIGNED
     ) prev
-  on dpo.ORDER_NUMBER = prev.ORDER_NUMBER
+  on dpo.UNIQUE_KEY = prev.UNIQUE_KEY
 )
